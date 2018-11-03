65a61bef74b6eddb9d0adda7d42750d974a78f53
matheus sousa <matheus.ads.study@gmail.com>
2018-11-03 09:33:10 -0300

    Criando nossa estrutura DAO de acesso ao banco de dados 2

--- src/main/java/dao/UserPostegresDAO.java
+++ src/main/java/dao/UserPostegresDAO.java
@@ -1,5 +1,15 @@
 package dao;
 
-public class UserPostegresDAO {
+import java.sql.Connection;
 
+import conexaojdbc.SingleConnection;
+
+public class UserPostegresDAO {
+	
+	private Connection connection; //Variável que recebe a conexão.
+	
+	public UserPostegresDAO() {//Construtor que cria a conexão.
+		connection = SingleConnection.getConection(); //Conexão instâciada.
+	}
+	
 }
