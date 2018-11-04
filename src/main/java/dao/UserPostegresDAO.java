cd9052bd83b56060e83252169ed2cd7c29f6d201
matheus sousa <matheus.ads.study@gmail.com>
2018-11-04 13:12:09 -0300

    Criar nosso INSERT ao banco de dados

--- src/main/java/dao/UserPostegresDAO.java
+++ src/main/java/dao/UserPostegresDAO.java
@@ -1,8 +1,10 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import conexaojdbc.SingleConnection;
+import model.Userpostegres;
 
 public class UserPostegresDAO {
 	
@@ -10,6 +12,30 @@ public class UserPostegresDAO {
 	
 	public UserPostegresDAO() {//Construtor que cria a conexão.
 		connection = SingleConnection.getConection(); //Conexão instâciada.
+	}
+	
+	public void salvar(Userpostegres obj) { // Vai salvar um registro recebendo um obj por parâmetro.
+		try {
+		String sql = "INSERT INTO userpostegres (id, nome, email) VALUES(?,?,?)"; // String que representa o SQL.
+		PreparedStatement insert = connection.prepareStatement(sql); //Prepara a instrução que antes é uma String.
+		insert.setLong(1, obj.getId());
+		insert.setString(2, obj.getNome());
+		insert.setString(3, obj.getEmail());
+		insert.execute();
+		
+		connection.commit(); //Salva no banco de dados.
+		
+		}catch(Exception e) {
+			
+			try {
+				connection.rollback();//Se algo der errado ele execulta um rollback que desfaz a operação de commit.
+			
+			}catch(Exception ex){
+				
+				ex.printStackTrace();
+			}
+			e.printStackTrace();
+		}
 	}
 	
 }
--- src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -2,14 +2,22 @@ package pos_java_jdbc.pos_java_jdbc;
 
 import org.junit.Test;
 
-import conexaojdbc.SingleConnection;
+import dao.UserPostegresDAO;
+import model.Userpostegres;
 
 public class TesteBancoJdbc {
 	
 	@Test //Anotação d teste
 	public void initBanco() { //Inicializa o banco.
 		
-		SingleConnection.getConection();
+		UserPostegresDAO userPostegresDAO = new UserPostegresDAO(); //Criando um dao junto com a conexão
+		Userpostegres userPostegres = new Userpostegres(); //Criando um novo obj
+		
+		userPostegres.setId(6L);
+		userPostegres.setNome("Sandra");
+		userPostegres.setEmail("sandra@gmail.com");
+		
+		userPostegresDAO.salvar(userPostegres);
 		
 	}
 
